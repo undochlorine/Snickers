806133f1eb9bb932d71696d5405995bb9f1e3d6c
alex-sikach <hlophlop1245@gmail.com>
2023-02-07 13:19:27 +0200

    function isTouched now just the product of getDistance one + .gitignore file

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,25 +1,3 @@
-function isTouched(el1, el2) {
-	const [x1_1, x1_2, y1_1, y1_2] = [el1.left, el1.left+el1.width, el1.top, el1.top+el1.height]
-	const [x2_1, x2_2, y2_1, y2_2] = [el2.left, el2.left+el2.width, el2.top,  el2.top+el2.height]
-	let horizontal = false, vertical = false;
-	for (let i = x1_1; i <= x1_2; i++) {
-		for (let j = x2_1; j <= x2_2; j++) {
-			if (i === j) {
-				horizontal = true;
-				break;
-			}
-		}
-	}
-	for (let i = y1_1; i <= y1_2; i++) {
-		for (let j = y2_1; j <= y2_2; j++) {
-			if (i === j) {
-				vertical = true;
-				break;
-			}
-		}
-	}
-	return horizontal && vertical;
-}
 function getDistance(el1, el2) {
 	let [left1, right1, bottom1] = [el1.left, el1.right, el1.bottom]
 	let [left2, right2, bottom2] = [el2.left, el2.right, el2.bottom]
@@ -32,6 +10,11 @@ function getDistance(el1, el2) {
 		horizontal,
 		vertical
 	}
+}
+
+function isTouched(el1, el2) {
+	const distance = getDistance(el1, el2)
+	return (distance.horizontal === 0 && distance.vertical === 0);
 }
 
 export { isTouched, getDistance }
